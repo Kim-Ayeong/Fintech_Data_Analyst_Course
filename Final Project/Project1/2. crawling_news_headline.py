e5dd3195bd804fb3496779af5e64d3581584c3f1
Kim Ayeong <rladkdud31@naver.com>
2020-02-07 12:04:58 +0900

    Add crawling file

---
+++ Final Project/Project1/2. crawling_news_headline.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  6 11:47:43 2020
+
+@author: Owner
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 19:08:13 2020
+
+@author: Owner
+"""
+import os
+from selenium import webdriver
+import bs4,re
+import pandas as pd
+import numpy as np
+import time
+
+os.chdir(r'C:\Users\Owner\Desktop\Minnie\project')
+
+driver1 = webdriver.Chrome("./chromedriver")
+
+# 검색 키워드
+search_list = ['담배']
+#search='여행'
+# 화면1
+for search in search_list:
+    driver1.get("https://search.naver.com/search.naver?where=news&query="+search+"&sm=tab_opt&sort=0&photo=0&field=0&reporter_article=&pd=3&ds=2017.11.01&de=2018.05.31&docid=&nso=so%3Ar%2Cp%3Afrom20171101to20180531%2Ca%3Aall&mynews=0&refresh_start=0&related=0")
+    
+
+    title=[]
+    contents=[]
+    while(True):
+
+        
+        # title 
+        driver1.find_elements_by_css_selector("ul.type01")
+        title_content=driver1.find_elements_by_css_selector("dl>dt>a")
+        for t in range(len(title_content)):
+            if title_content[t].text =='도움말':
+                continue
+            else:
+                title.append(title_content[t].text)
+         
+        # contents
+        driver1.find_elements_by_css_selector("ul.type01")
+        contents_list=driver1.find_elements_by_xpath("//*/dl/dd[2]")
+        for i in range(len(contents_list)):
+            if contents_list[i].text =='':
+                continue
+                
+            else:
+                ob=contents_list[i].text
+                ob=ob.replace("=","")
+                contents.append(ob)
+            
+        try:
+            driver1.find_element_by_css_selector("a.next").click()
+        except:
+            break
+    
+    news={"title":title,"contents":contents}
+
+
+    df_news=pd.DataFrame(news,columns=['title','contents'])
+    
+    df_news.to_excel(search+"_test.xlsx")
